2154165d2afbd76f84c00034d4ba8cc893374fd5
Seifeddine Touj <seifeddine.touj1@gmail.com>
2024-04-13 00:48:24 +0200

    (back) booking logic

--- hotel-back/src/main/java/igl/projet/hotel/controller/ReservationController.java
+++ hotel-back/src/main/java/igl/projet/hotel/controller/ReservationController.java
@@ -1,16 +1,15 @@
 package igl.projet.hotel.controller;
 
 import igl.projet.hotel.model.Reservation;
-import igl.projet.hotel.model.Room;
 import igl.projet.hotel.repository.ReservationRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/reservations")
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600, allowCredentials="true")
 public class ReservationController {
 
     @Autowired
@@ -19,6 +18,11 @@ public class ReservationController {
     @PostMapping("/add")
     public void addReservation(@RequestBody Reservation reservation){
         reservationRepo.save(reservation);
+    }
+
+    @GetMapping("/show")
+    public List<Reservation> getReservations(){
+        return reservationRepo.findAll();
     }
 
 }
